e0baea4d4b92c04c54b58332ab0c3c2c491e99b1
tomas <tomaszkacperkulig@gmail.com>
2021-08-29 01:30:23 +0200

    Sending working

--- src/main/kotlin/Sender.kt
+++ src/main/kotlin/Sender.kt
@@ -14,17 +14,17 @@ fun main() {
 
     val web3service = HttpService("https://polygon-mainnet.infura.io/v3/fac98e56ea7e49608825dfc726fab703")
     val web3 = JsonRpc2_0Web3j(web3service)
-    val keys = ECKeyPair.create(
-        BigInteger("82596650919171886625369893250123334326338557372459095847543019503875525979057")
-    )
+
+    val keys = ECKeyPair.create( BigInteger("73879472168346952077578437103592564545953740477884952342696959757500830031267"))
+    println(keys.privateKey)
     //val keys = Keys.createEcKeyPair()
     val creds = Credentials.create(keys)
     println(creds.address)
     val transactionManager = RawTransactionManager(web3, creds, 137)
 
-    val gasPrice = BigInteger.valueOf(220000000000L)
-    val gasLimit = BigInteger.valueOf(50000)
-    val value = BigInteger.valueOf(500000000000)
+    val gasPrice = BigInteger.valueOf(22000000000L)
+    val gasLimit = BigInteger.valueOf(40000)
+    val value = BigInteger.valueOf(500000000)
     val hash = transactionManager.sendTransaction(
         gasPrice,
         gasLimit,
