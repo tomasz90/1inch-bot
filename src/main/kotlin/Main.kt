91b01786590724a9d9fc30ce8ca1318e43904665
tomas <tomaszkacperkulig@gmail.com>
2021-08-28 15:00:35 +0200

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,25 @@ fun main() {
     val oneInchClient = OneInchClient()
     repeat(10) {
         oneInchClient.getQuote(BSC_DAI, BSC_UST, AMOUNT_TO_SELL)
+        oneInchClient.getQuote(BSC_DAI, BSC_USDT, AMOUNT_TO_SELL)
+        oneInchClient.getQuote(BSC_DAI, BSC_USDC, AMOUNT_TO_SELL)
+        oneInchClient.getQuote(BSC_DAI, BSC_TUSD, AMOUNT_TO_SELL)
+
+        oneInchClient.getQuote(BSC_UST, BSC_DAI, AMOUNT_TO_SELL)
+        oneInchClient.getQuote(BSC_UST, BSC_USDT, AMOUNT_TO_SELL)
+        oneInchClient.getQuote(BSC_UST, BSC_USDC, AMOUNT_TO_SELL)
+        oneInchClient.getQuote(BSC_UST, BSC_TUSD, AMOUNT_TO_SELL)
+
+        oneInchClient.getQuote(BSC_USDT, BSC_UST, AMOUNT_TO_SELL)
+        oneInchClient.getQuote(BSC_USDT, BSC_DAI, AMOUNT_TO_SELL)
+        oneInchClient.getQuote(BSC_USDT, BSC_USDC, AMOUNT_TO_SELL)
+        oneInchClient.getQuote(BSC_USDT, BSC_TUSD, AMOUNT_TO_SELL)
+
+        oneInchClient.getQuote(BSC_TUSD, BSC_DAI, AMOUNT_TO_SELL)
+        oneInchClient.getQuote(BSC_TUSD, BSC_USDT, AMOUNT_TO_SELL)
+        oneInchClient.getQuote(BSC_TUSD, BSC_USDC, AMOUNT_TO_SELL)
+        oneInchClient.getQuote(BSC_TUSD, BSC_UST, AMOUNT_TO_SELL)
+
         TimeUnit.SECONDS.sleep(5L)
     }
     exitProcess(0)
