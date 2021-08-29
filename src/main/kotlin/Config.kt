8050636b817b8b6b92c9e3bb881a9431fc075055
tomas <tomaszkacperkulig@gmail.com>
2021-08-29 22:35:55 +0200

    changed Long to double of initial amount

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -4,8 +4,9 @@ object Config {
 
     val CHAIN = MATIC
     const val MY_ADDRESS = "0x0bd44B1908678c9CD40b70c0C18F395Eb9093622"
-    const val AMOUNT_TO_SELL = 50000.0
+    const val AMOUNT_TO_SELL = 0.1
     const val DEMAND_PERCENT_ADVANTAGE = 0.5
     const val MAX_SLIPPAGE = 0.5
+    const val LOG_DECIMAL_PRECISION = 3
 
 }
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,3 +1,4 @@
+import Config.LOG_DECIMAL_PRECISION
 import org.apache.log4j.FileAppender
 import org.apache.log4j.LogManager
 import org.slf4j.Logger
@@ -5,7 +6,6 @@ import org.slf4j.LoggerFactory
 import quote_request.QuoteResponse
 import java.math.BigDecimal
 import java.math.BigInteger
-import kotlin.math.pow
 
 fun getLogger(): Logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)
 
@@ -17,8 +17,8 @@ fun withCleanLog(boolean: Boolean) {
 }
 
 fun calculateAdvantage(response: QuoteResponse): Float {
-    val fromFloat = cut(response.fromTokenAmount, response.fromToken.decimals).toFloat()
-    val toFloat = cut(response.toTokenAmount, response.toToken.decimals).toFloat()
+    val fromFloat = cut(response.fromTokenAmount, response.fromToken.decimals)
+    val toFloat = cut(response.toTokenAmount, response.toToken.decimals)
     return (toFloat - fromFloat) / fromFloat * 100
 }
 
@@ -27,18 +27,20 @@ fun logRatesInfo(response: QuoteResponse, percent: Float) {
     val toDec = cut(response.toTokenAmount, response.toToken.decimals)
     getLogger()
         .info(
-            "${response.fromToken.symbol}: $fromDec, ${response.toToken.symbol}: $toDec," +
-                    "   advantage: ${String.format("%.2f", percent)}%"
+            "${response.fromToken.symbol}: ${fromDec.precision()}, ${response.toToken.symbol}: ${toDec.precision()}," +
+                    "   advantage: ${percent.precision(2)}"
         )
 }
 
 // adjusts quotes to token standard, from being more readable
-fun expand(quote: Double, decimals: Int): BigInteger {
+fun expand(quote: Double, decimals: Int): BigInteger {6
     return BigDecimal.valueOf(quote).multiply(BigInteger.valueOf(10L).pow(decimals).toBigDecimal()).toBigInteger()
 }
 // adjust quotes to be more readable
-fun cut(quote: BigInteger, decimals: Int): Long {
-    return (quote/BigInteger.valueOf(10).pow(decimals)).toLong()
+fun cut(quote: BigInteger, decimals: Int): Float {
+    return quote.toFloat()/(BigInteger.valueOf(10).pow(decimals)).toFloat()
 }
+
+fun Float.precision(int: Int = LOG_DECIMAL_PRECISION) = String.format("%.${int}f", this)
 
 
