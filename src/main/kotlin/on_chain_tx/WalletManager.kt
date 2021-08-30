57ec93df4f7e94c5f0adf34e3cc5b70fd84cd73c
tomas <tomaszkacperkulig@gmail.com>
2021-08-30 19:05:24 +0200

    Fixed wallet problems

--- src/main/kotlin/on_chain_tx/WalletManager.kt
+++ src/main/kotlin/on_chain_tx/WalletManager.kt
@@ -1,21 +1,31 @@
 package on_chain_tx
 
-import getLogger
-import org.web3j.crypto.Credentials
-import org.web3j.crypto.WalletUtils
+import org.web3j.crypto.*
+import org.web3j.crypto.Bip32ECKeyPair.HARDENED_BIT
 import java.io.File
 import java.io.FilenameFilter
+import java.security.SecureRandom
 
 class WalletManager {
 
-    fun generateWallet(password: String) {
-        val file = File(System.getProperty("user.dir"))
-        file.createNewFile()
-        WalletUtils.generateBip39Wallet(password, file)
+    private val file = File(System.getProperty("user.dir"))
+
+    fun generateMnemonic(): String {
+        val initialEntropy = ByteArray(16)
+        SecureRandom().nextBytes(initialEntropy)
+        return MnemonicUtils.generateMnemonic(initialEntropy)
+    }
+
+    fun generateWalletFromMnemonic(mnemonic: String, password: String) {
+        // "m/44'/60'/0'/0/0"
+        val path = intArrayOf(44 or HARDENED_BIT, 60 or HARDENED_BIT, 0 or HARDENED_BIT, 0, 0)
+        val seed = MnemonicUtils.generateSeed(mnemonic,"")
+        val masterKeyPair = Bip32ECKeyPair.generateKeyPair(seed)
+        val bip44Keypair = Bip32ECKeyPair.deriveKeyPair(masterKeyPair, path)
+        WalletUtils.generateWalletFile(password, bip44Keypair, file, true)
     }
 
     fun openWallet(): Credentials {
-        val file = File(System.getProperty("user.dir"))
         val filter = FilenameFilter { _: File?, name: String -> name.startsWith("UTC") && name.endsWith(".json") }
         val files = file.listFiles(filter)!!
         if (files.isEmpty()) {
@@ -25,4 +35,9 @@ class WalletManager {
 //        val password = (System.console()?.readPassword() ?: readLine()).toString()
         return WalletUtils.loadCredentials("***REMOVED***", files[0])
     }
+}
+
+fun main() {
+
+println(WalletManager().generateMnemonic())
 }
