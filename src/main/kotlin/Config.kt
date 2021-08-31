2d18d791332db068b10e4c44e7f7d1e57f4e76d4
tomas <tomaszkacperkulig@gmail.com>
2021-08-31 14:20:58 +0200

    small changes

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -4,9 +4,11 @@ object Config {
 
     val CHAIN: Chain = MATIC
     const val MY_ADDRESS = "0x0fad488c45e44B72A17e4eBFc20ce16ff284de3E"
+    const val LOG_DECIMAL_PRECISION = 0
+
     const val AMOUNT_TO_SELL: Double = 20000.0
     const val DEMAND_PERCENT_ADVANTAGE: Double = 1.0
     const val MAX_SLIPPAGE: Double = 0.1
-    const val LOG_DECIMAL_PRECISION = 0
+    const val INCREASED_GAS_LIMIT = 1.25
 
 }
--- src/main/kotlin/on_chain_tx/Sender.kt
+++ src/main/kotlin/on_chain_tx/Sender.kt
@@ -1,6 +1,7 @@
 package on_chain_tx
 
 import Config.CHAIN
+import Config.INCREASED_GAS_LIMIT
 import getLogger
 import org.web3j.protocol.core.JsonRpc2_0Web3j
 import org.web3j.protocol.http.HttpService
@@ -15,9 +16,13 @@ class Sender {
     private val manager = RawTransactionManager(web3j, credentials, CHAIN.id.toLong())
 
     fun sendTransaction(gasPrice: BigInteger, gasLimit: BigInteger, value: BigInteger, address: String, data: String) {
-        val increasedGasLimit = (gasLimit.toDouble() * 1.25).toBigDecimal().toBigInteger()
+        val increasedGasLimit = increaseGasLimit(gasLimit)
         getLogger().info("Swapping, gasPrice: $gasPrice gasLimit: $increasedGasLimit")
         val tx = manager.sendTransaction(gasPrice, increasedGasLimit, address, data, value)
         getLogger().info("TxHash: ${tx.transactionHash}")
+    }
+
+    private fun increaseGasLimit(gasLimit: BigInteger): BigInteger {
+        return (gasLimit.toDouble() * INCREASED_GAS_LIMIT).toBigDecimal().toBigInteger()
     }
 }
