9231bc393177e5c6a48e52fc4b18b89c9d54c182
tomas <tomaszkacperkulig@gmail.com>
2021-09-05 14:24:07 +0200

    working, deleted bigdecimals

--- src/main/kotlin/com/oneinch/repository/RealRepositoryManager.kt
+++ src/main/kotlin/com/oneinch/repository/RealRepositoryManager.kt
@@ -2,7 +2,9 @@ package com.oneinch.repository
 
 import com.oneinch.`object`.TokenQuote
 import com.oneinch.on_chain_api.tx.Transaction
+import org.springframework.stereotype.Component
 
+@Component
 class RealRepositoryManager: IRepositoryManager {
 
     fun saveTransaction(from: TokenQuote, to: TokenQuote, t: Transaction, txHash: String) {
