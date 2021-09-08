95e3f1ef9c19b64a4bcd82e3e53801d39140e2ae
tomas <tomaszkacperkulig@gmail.com>
2021-09-08 12:10:09 +0200

    Some refactor

--- src/main/kotlin/com/oneinch/Main.kt
+++ src/main/kotlin/com/oneinch/Main.kt
@@ -3,6 +3,7 @@ package com.oneinch
 import com.oneinch.`object`.Chain
 import com.oneinch.`object`.Token
 import com.oneinch.config.Settings
+import com.oneinch.on_chain_api.balance.Balance
 import com.oneinch.on_chain_api.balance.IBalance
 import com.oneinch.one_inch_api.requester.AbstractRequester
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,19 +11,7 @@ import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit
 
 @Component
-class Main {
-
-    @Autowired
-    private lateinit var requester: AbstractRequester
-
-    @Autowired
-    private lateinit var balance: IBalance
-
-    @Autowired
-    private lateinit var chain: Chain
-
-    @Autowired
-    private lateinit var settings: Settings
+class Main(val requester: AbstractRequester, val balance: IBalance, val chain: Chain, val settings: Settings) {
 
     fun run() {
         val pairs = createUniquePairs(chain.tokens)
