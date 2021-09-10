20855a1c902ad9d35f69fa0450318a649e273e61
tomas <tomaszkacperkulig@gmail.com>
2021-09-10 21:55:02 +0200

    remove unneeded params

--- src/main/kotlin/com/oneinch/Main.kt
+++ src/main/kotlin/com/oneinch/Main.kt
@@ -6,30 +6,38 @@ import com.oneinch.config.Settings
 import com.oneinch.on_chain_api.balance.Balance
 import com.oneinch.on_chain_api.balance.IBalance
 import com.oneinch.one_inch_api.requester.AbstractRequester
+import io.github.resilience4j.ratelimiter.RateLimiter
+import io.github.resilience4j.ratelimiter.RateLimiterConfig
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry
+import kotlinx.coroutines.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import java.time.Duration
 import java.util.concurrent.TimeUnit
 
 @Component
 class Main(val requester: AbstractRequester, val balance: IBalance, val chain: Chain, val settings: Settings) {
-
+    val coroutine = CoroutineScope(CoroutineName("coroutine"))
     fun run() {
         val pairs = createUniquePairs(chain.tokens)
-
-        while (true) {
-            checkRatesForEveryPair(pairs)
-            getLogger().info("---------------- WAIT ----------------")
-            TimeUnit.SECONDS.sleep(settings.sleepTime)
+        runBlocking {
+            while (true) {
+                checkRatesForEveryPair(pairs)
+            }
         }
     }
 
-    private fun checkRatesForEveryPair(pairs: List<Pair<Token, Token>>) {
-        pairs.forEach { pair -> checkRatesForPair(pair) }
+    private suspend fun checkRatesForEveryPair(pairs: List<Pair<Token, Token>>) {
+        pairs.forEach { pair ->
+            coroutine.launch { checkRatesForPair(pair) }
+        }
+        delay(500)
     }
 
     private fun checkRatesForPair(pair: Pair<Token, Token>) {
         when (val tokenQuote = balance.getERC20(pair.first)) {
-            null -> { }
+            null -> {
+            }
             else -> {
                 if (tokenQuote.calcReadable(chain) > settings.minimalSwapQuote) {
                     requester.swap(tokenQuote, pair.second)
