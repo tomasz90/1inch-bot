14abb8b9fa92ad5cbedb1b2940b56434886fddbf
tomas <tomaszkacperkulig@gmail.com>
2021-09-12 14:57:41 +0200

    some fixes

--- src/main/kotlin/com/oneinch/one_inch_api/api/OneInchApi.kt
+++ src/main/kotlin/com/oneinch/one_inch_api/api/OneInchApi.kt
@@ -26,7 +26,7 @@ interface OneInchApi {
         @Query("amount") amount: BigInteger,
         @Query("fromAddress") fromAddress: String,
         @Query("slippage") slippage: Double,
-        @Query("gasPrice") gasPrice: BigInteger? = null,
-        @Query("allowPartialFill") allowPartialFill: Boolean? = null
+        @Query("allowPartialFill") allowPartialFill: Boolean = true,
+        @Query("gasPrice") gasPrice: BigInteger? = null
     ): Call<SwapResponse>
 }
