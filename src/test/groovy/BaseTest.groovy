81ae3875ab179421867d00e92e613f83db74efd0
tomas <tomaszkacperkulig@gmail.com>
2021-09-21 11:18:26 +0200

    basetest working

--- src/test/groovy/BaseTest.groovy
+++ src/test/groovy/BaseTest.groovy
@@ -2,15 +2,21 @@ import com.esaulpaugh.headlong.abi.Function
 import com.github.openjson.JSONObject
 import com.oneinch.config.AbiLoader
 import com.oneinch.util.SlippageModifier
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Import
 
 @TestConfiguration
+@Import(AbiLoader.class)
 abstract class BaseTest {
+
+    @Autowired
+    AbiLoader abiLoader
 
     @Bean
     Function function() {
-        JSONObject json = new AbiLoader().load()
+        JSONObject json = abiLoader.load()
         return Function.fromJson(json.toString())
     }
 
