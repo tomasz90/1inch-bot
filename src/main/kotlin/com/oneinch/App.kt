772c6d5c5f4bc708e93e157478146741dca663f3
tomas <tomaszkacperkulig@gmail.com>
2021-09-28 12:06:16 +0200

    working

--- src/main/kotlin/com/oneinch/App.kt
+++ src/main/kotlin/com/oneinch/App.kt
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.core.env.ConfigurableEnvironment
 import org.springframework.core.env.StandardEnvironment
@@ -19,6 +21,7 @@ open class App : CommandLineRunner {
     lateinit var main: Main
 
     companion object {
+        lateinit var profile: String
         lateinit var context: ConfigurableApplicationContext
 
         @JvmStatic
@@ -26,7 +29,7 @@ open class App : CommandLineRunner {
             val args = context.getBean(ApplicationArguments::class.java)
             val thread = Thread {
                 context.close()
-                context = setEnvironment().run(*args.sourceArgs)
+                context = setEnvironment(profile).run(*args.sourceArgs)
             }
             thread.isDaemon = false
             thread.start()
@@ -39,18 +42,23 @@ open class App : CommandLineRunner {
 }
 
 fun main(args: Array<String>) {
-    val application = setEnvironment()
+    val profile = SettingsLoader.load().account
+    readPasswordIfRealProfile(profile)
+    val application = setEnvironment(profile)
+    App.profile = profile
     App.context = application.run(*args)
 }
 
-private fun setEnvironment(): SpringApplication {
-    val profile = SettingsLoader.load().account
-    if (profile == "realAccount") {
-        Wallet.assignPassword()
-    }
+private fun setEnvironment(profile: String): SpringApplication {
     val environment: ConfigurableEnvironment = StandardEnvironment()
     environment.setActiveProfiles(profile)
     val application = SpringApplication(App::class.java)
     application.setEnvironment(environment)
     return application
+}
+
+private fun readPasswordIfRealProfile(profile: String) {
+    if (profile == "realAccount") {
+        Wallet.assignPassword()
+    }
 }
